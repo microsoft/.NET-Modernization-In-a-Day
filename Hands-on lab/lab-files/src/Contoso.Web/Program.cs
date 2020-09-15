8b9ffdd6eb2470c70ab1dfe90ab45460d978e645
Kristinn Gunnarsson <kristinn.gunnarsson@solidify.dev>
2020-09-15 19:39:47 +0200

    implemented Azure Keyvault in Web

--- Hands-on lab/lab-files/src/Contoso.Web/Program.cs
+++ Hands-on lab/lab-files/src/Contoso.Web/Program.cs
@@ -3,6 +3,7 @@ using Contoso.Web.Configuration;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using System.Configuration;
 
 namespace Contoso.Web
 {
@@ -17,9 +18,11 @@ namespace Contoso.Web
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((context, config) =>
                 {
-                    var buildConfig = config.Build();
-
+                var buildConfig = config.Build();
                     config.AddEnvironmentVariables();
+                    if (string.IsNullOrEmpty(buildConfig["KeyVaultName"]) || string.IsNullOrEmpty(buildConfig["KeyVaultClientId"]) || string.IsNullOrEmpty(buildConfig["KeyVaultClientSecret"]))
+                        throw new ConfigurationErrorsException("one or all of the configuration settings for Azure key vault are missing");
+                    config.AddAzureKeyVault(KeyVaultConfig.GetKeyVaultEndpoint(buildConfig["KeyVaultName"]), buildConfig["KeyVaultClientId"], buildConfig["KeyVaultClientSecret"]);                    
                 })
                 .UseStartup<Startup>();
     }
